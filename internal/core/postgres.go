a3d04af2291bd87fe7e420e81bb054223c850592
Bhargav Mahanta <bhargavmahanta30@gmail.com>
2026-08-29 12:30:12 +0530

    feat: persist incidents and graphs transactionally

--- internal/core/postgres.go
+++ internal/core/postgres.go
@@ -29,3 +29,29 @@ func (r *PostgresRepository) IngestEvent(ctx context.Context, e contracts.Execut
 	}
 	return tx.Commit()
 }
+
+func (r *PostgresRepository) PutIncident(ctx context.Context, i contracts.Incident, g contracts.ExecutionGraph) error {
+	if i.SchemaVersion != "1.0" || i.IncidentID == "" {
+		return fmt.Errorf("invalid incident")
+	}
+	ip, err := json.Marshal(i)
+	if err != nil {
+		return err
+	}
+	gp, err := json.Marshal(g)
+	if err != nil {
+		return err
+	}
+	tx, err := r.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+	if _, err = tx.ExecContext(ctx, `INSERT INTO incidents(incident_id,status,payload) VALUES($1,$2,$3)`, i.IncidentID, i.Status, ip); err != nil {
+		return fmt.Errorf("insert incident: %w", err)
+	}
+	if _, err = tx.ExecContext(ctx, `INSERT INTO execution_graphs(graph_id,incident_id,payload) VALUES($1,$2,$3)`, g.GraphID, i.IncidentID, gp); err != nil {
+		return fmt.Errorf("insert graph: %w", err)
+	}
+	return tx.Commit()
+}
